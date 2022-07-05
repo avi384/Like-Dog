71d7282311bd92e5d5faa32218a84f9e9a6e8433
Abhishek Thakur <107882805+avi384@users.noreply.github.com>
2022-07-05 22:08:14 +0530

    PND code

--- solidity/Context.sol
+++ solidity/Context.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+// SPDX-License-Identifier: GNU GPLv2
+// Contracts v4.4.1 (utils/Context.sol)
 
 pragma solidity 0.5.16;
 
