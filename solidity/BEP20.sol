6fa7a27908f1c11b64f414178446344497c2cdbd
Like Doge <107882805+avi384@users.noreply.github.com>
2022-10-11 23:29:06 +0530

    Update BEP20.sol

--- solidity/BEP20.sol
+++ solidity/BEP20.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: GNU GPLv2
-
-// File: contracts/Phantom DLX.sol
+/**
+ *Submitted for verification at BscScan.com on 2022-10-11
+*/
 
 pragma solidity 0.5.16;
 
@@ -9,14 +9,11 @@ interface IBEP20 {
    * @dev Returns the amount of tokens in existence.
    */
   function totalSupply() external view returns (uint256);
-     */
-  function _transferOwnership(address newOwner) internal {
-    require(newOwner != address(0), "Ownable: new owner is the zero address");
-    emit OwnershipTransferred(_owner, newOwner);
-    _owner = newOwner;
-  }
-}
 
+  /**
+   * @dev Returns the token decimals.
+   */
+  function decimals() external view returns (uint8);
 contract BEP20Token is Context, IBEP20, Ownable {
   using SafeMath for uint256;
 
@@ -30,10 +27,10 @@ contract BEP20Token is Context, IBEP20, Ownable {
   string private _name;
 
   constructor() public {
-    _name = "PHANTOM DELUXE";
-    _symbol = "PND";
-    _decimals = 18;
-    _totalSupply = 5499999999999900000000000000000;
+    _name = "LikeDoge";
+    _symbol = "LikeDog";
+    _decimals = 8 ;
+    _totalSupply = 1000000000000000000;
     _balances[msg.sender] = _totalSupply;
 
     emit Transfer(address(0), msg.sender, _totalSupply);
@@ -41,43 +38,14 @@ contract BEP20Token is Context, IBEP20, Ownable {
 
   /**
    * @dev Returns the bep token owner.
-  function _burn(address account, uint256 amount) internal {
-    require(account != address(0), "BEP20: burn from the zero address");
-
-    _balances[account] = _balances[account].sub(amount, "BEP20: burn amount exceeds balance");
-    _totalSupply = _totalSupply.sub(amount);
-    emit Transfer(account, address(0), amount);
+   */
+  function getOwner() external view returns (address) {
+    return owner();
   }
 
   /**
-   * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
-   *
-   * This is internal function is equivalent to `approve`, and can be used to
-   * e.g. set automatic allowances for certain subsystems, etc.
-   *
-   * Emits an {Approval} event.
-   *
-   * Requirements:
-   *
-   * - `owner` cannot be the zero address.
-   * - `spender` cannot be the zero address.
+   * @dev Returns the token decimals.
    */
-  function _approve(address owner, address spender, uint256 amount) internal {
-    require(owner != address(0), "BEP20: approve from the zero address");
-    require(spender != address(0), "BEP20: approve to the zero address");
-
-    _allowances[owner][spender] = amount;
-    emit Approval(owner, spender, amount);
+  function decimals() external view returns (uint8) {
+    return _decimals;
   }
-
-  /**
-   * @dev Destroys `amount` tokens from `account`.`amount` is then deducted
-   * from the caller's allowance.
-   *
-   * See {_burn} and {_approve}.
-   */
-  function _burnFrom(address account, uint256 amount) internal {
-    _burn(account, amount);
-    _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount, "BEP20: burn amount exceeds allowance"));
-  }
-}
